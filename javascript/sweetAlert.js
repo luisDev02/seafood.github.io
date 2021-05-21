20eaf194b0534208485b5a0b12dcaf81c80fa04a
luis <legan.01apolinar@gmail.com>
2021-05-20 20:26:06 -0600

    datos

---
+++ javascript/sweetAlert.js
@@ -0,0 +1,13 @@
+Swal.fire({
+    html:'Wellcome the Restaurant Pancho´s ',
+    width:'25%',
+    showConfirmButton:false,
+    padding:'2rem',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+    
+  })
