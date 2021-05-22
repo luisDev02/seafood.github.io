b09b86d390b4653c9d5f08147c103c2d4c353fd1
luis <legan.01apolinar@gmail.com>
2021-05-21 22:24:22 -0600

    correcciones

--- javascript/scroll.js
+++
@@ -1,9 +0,0 @@
-function ScrollAppear() {
-    var container_allother =document.querySelector('.container-allother');
-    var introPosition = container_allother.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight/1.3;
-
-    if (introPosition<screenPosition) {
-        container_allother.classList.add('intro_appear');
-    }
-}
