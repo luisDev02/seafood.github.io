b34bf388ddc68b3712f46772aabc50ec5df10784
luis <legan.01apolinar@gmail.com>
2021-05-21 20:25:30 -0600

    otra vez scroll

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,7 +1,8 @@
 function ScrollAppear() {
     var container_allother =document.querySelector('.container_allother');
     var introPosition = container_allother.getBoundingClientRect().top;
-    var screenPosition =window.innerHeight/1.3;
+    var screenPosition = window.innerHeight/1.3;
+
     if (introPosition<screenPosition) {
         container_allother.classList.add('intro_appear');
     }
